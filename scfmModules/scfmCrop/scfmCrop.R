cc915380a81fc0accc4933a4c4cd32807c1aaa3d
emcintir <eliotmcintire@gmail.com>
2017-05-29 18:06:16 -0700

    Cache -- use Cache throughout -- remove sim cached files

--- scfmModules/scfmCrop/scfmCrop.R
+++ scfmModules/scfmCrop/scfmCrop.R
@@ -64,31 +64,51 @@ scfmCropInit <- function(sim) {
   
   #Project the study area into each input raster, then crop and mask; 
   #Then project result intpo sim projection.
- 
-  studyAreaTmp <- Cache(spTransform, sim$studyArea, CRSobj =vegProjection)
-  sim$vegMap <-  Cache(crop, sim$vegMapInit, studyAreaTmp)
-  crs(sim$vegMap) <- vegProjection
-  sim$vegMap <- Cache(mask, sim$vegMap,studyAreaTmp) #
-  sim$vegMap <- Cache(projectRaster, sim$vegMap,crs=simProjection,method="ngb")
-  sim$Mask <- sim$vegMap
-  sim$Mask[] <- ifelse(is.na(sim$vegMap[]), NA, 1)
+  cropMaskReproject <- function(map, studyArea, fromProjection, toProjection) {
+    studyAreaTmp <- spTransform(studyArea, CRSobj=fromProjection)
+    updatedMap <-  Cache(crop, map, studyAreaTmp)
+    crs(updatedMap) <- fromProjection
+    updatedMap <- Cache(mask, updatedMap, studyAreaTmp) #
+    updatedMap <- Cache(projectRaster, updatedMap,
+                    crs=toProjection, method="ngb")
+    
+  }
+  
+  sim$vegMap <- Cache(cropMaskReproject, sim$vegMapInit, sim$studyArea,
+                                  vegProjection, simProjection)
+  
+  buildBinaryMask <- function(map) {
+    Mask <- map
+    Mask[!is.na(map[])] <- 1
+    Mask
+  }
+  sim$Mask <- Cache(buildBinaryMask, sim$vegMap)
   
   tmp <- getColors(sim$vegMapInit)[[1]] # mask removes colors!
-  setColors(sim$vegMap, n=length(tmp)) <- tmp 
+  setColors(sim$vegMap, n=length(tmp)-1) <- tmp[-1]
+  #setColors(sim$vegMap, n=maxValue(sim$vegMap)) <- tmp[1:maxValue(sim$vegMap)]
   
   ageProjection <- crs(sim$ageMapInit)
-  studyAreaTmp <- Cache(spTransform, sim$studyArea, CRSobj =ageProjection)
-  sim$ageMap <-  Cache(crop, sim$ageMapInit, studyAreaTmp)
-  crs(sim$ageMap) <- ageProjection
-  sim$ageMap <- Cache(mask, sim$ageMap,studyAreaTmp)
-  sim$ageMap <- Cache(projectRaster, sim$ageMap,to=sim$vegMap,method="ngb")
+  sim$ageMap <- Cache(cropMaskReproject, sim$ageMapInit, sim$studyArea,
+                      ageProjection, simProjection)
   
-  fireProjection <- CRS(proj4string(sim$firePointsInput))
-  studyAreaTmp <- Cache(spTransform, sim$studyArea, CRSobj =fireProjection)
-  sim$firePoints <- sim$firePointsInput[studyAreaTmp,]  #note possibly correct syntax A[B,] rather than A[B]
-                                                        #https://cran.r-project.org/web/packages/sp/vignettes/over.pdf page 3
-  #crs(sim$firePoints) <- fireProjection
-  sim$firePoints <- Cache(spTransform, sim$firePoints,CRSobj =simProjection)
+  # studyAreaTmp <- Cache(spTransform, sim$studyArea, CRSobj =ageProjection)
+  # sim$ageMap <-  Cache(crop, sim$ageMapInit, studyAreaTmp)
+  # crs(sim$ageMap) <- ageProjection
+  # sim$ageMap <- Cache(mask, sim$ageMap,studyAreaTmp)
+  # sim$ageMap <- Cache(projectRaster, sim$ageMap,to=sim$vegMap,method="ngb")
+  
+  cropMaskReprojectSP <- function(sp, studyArea, toProjection) {
+    fireProjection <- CRS(proj4string(sp))
+    studyAreaTmp <- Cache(spTransform, studyArea, CRSobj =fireProjection)
+    firePoints <- sp[studyAreaTmp,]  #note possibly correct syntax A[B,] rather than A[B]
+    #https://cran.r-project.org/web/packages/sp/vignettes/over.pdf page 3
+    #crs(sim$firePoints) <- fireProjection
+    firePoints <- Cache(spTransform, firePoints,CRSobj=toProjection)
+  }
+  
+  sim$firePoints <- Cache(cropMaskReprojectSP, sim$firePointsInput, 
+                          sim$studyArea, simProjection)
   
   #endCluster()
     
@@ -98,40 +118,46 @@ scfmCropInit <- function(sim) {
   #age.crsAge <- mask(age.crsAge, spTransform(sim$studyArea, CRSobj = crs(sim$age)))
   #sim$ageMapInit <- projectRaster(age.crsAge, to=sim$vegMap, method="ngb")
   
-  if(sum(!is.na(getValues(sim$ageMap)))==0) stop("There are no age data provided with input age map")
-  if(sum(!is.na(getValues(sim$vegMap)))==0) stop("There are no vegatation data provided with input vegatation map")
+  
+  
+  #if(sum(!is.na(getValues(sim$ageMap)))==0) stop("There are no age data provided with input age map")
+  #if(sum(!is.na(getValues(sim$vegMap)))==0) stop("There are no vegatation data provided with input vegatation map")
+  
+  # Faster -- and should be as robust since raster functions update their maxValue to NA if all are NA
+  if(is.na(maxValue(sim$ageMap))) stop("There are no age data provided with input age map")
+  if(is.na(maxValue(sim$vegMap))) stop("There are no vegatation data provided with input vegatation map")
   
   return(invisible(sim))
 }
 
 
-scfmCropCacheFunctions <- function(sim) {
-  # for slow functions, add cached versions
-  if(params(sim)$scfmCrop$useCache) {
-    sim$cacheLoc <- file.path(outputPath(sim), "scfmCropCache") 
-    
-    if(!dir.exists(sim$cacheLoc) )
-      createEmptyRepo(file.path(outputPath(sim), "scfmCropCache"))
-    
-    sim$mask <- function(...) {
-      archivist::cache(cacheRepo=sim$cacheLoc, FUN=raster::mask, ...)
-    }
-    sim$crop <- function(...) {
-      archivist::cache(cacheRepo=sim$cacheLoc, FUN=raster::crop, ...)
-    }
-    sim$projectRaster <- function(...) {
-      archivist::cache(cacheRepo=sim$cacheLoc, FUN=raster::projectRaster, ...)
-    }
-    sim$spTransform <- function(...) {
-      archivist::cache(cacheRepo=sim$cacheLoc, FUN=sp::spTransform,  ...)
-    }
-  } else {
-    sim$mask <- raster::mask
-    sim$crop <- raster::crop
-    sim$projectRaster <- raster::projectRaster
-    sim$spTransform <- sp::spTransform
-    
-  }
-  
-  return(invisible(sim))
-}
+# scfmCropCacheFunctions <- function(sim) {
+#   # for slow functions, add cached versions
+#   if(params(sim)$scfmCrop$useCache) {
+#     sim$cacheLoc <- file.path(outputPath(sim), "scfmCropCache") 
+#     
+#     if(!dir.exists(sim$cacheLoc) )
+#       createEmptyRepo(file.path(outputPath(sim), "scfmCropCache"))
+#     
+#     sim$mask <- function(...) {
+#       archivist::cache(cacheRepo=sim$cacheLoc, FUN=raster::mask, ...)
+#     }
+#     sim$crop <- function(...) {
+#       archivist::cache(cacheRepo=sim$cacheLoc, FUN=raster::crop, ...)
+#     }
+#     sim$projectRaster <- function(...) {
+#       archivist::cache(cacheRepo=sim$cacheLoc, FUN=raster::projectRaster, ...)
+#     }
+#     sim$spTransform <- function(...) {
+#       archivist::cache(cacheRepo=sim$cacheLoc, FUN=sp::spTransform,  ...)
+#     }
+#   } else {
+#     sim$mask <- raster::mask
+#     sim$crop <- raster::crop
+#     sim$projectRaster <- raster::projectRaster
+#     sim$spTransform <- sp::spTransform
+#     
+#   }
+#   
+#   return(invisible(sim))
+# }
--- scfmModules/scfmLandcoverInit/scfmLandcoverInit.R
+++ scfmModules/scfmLandcoverInit/scfmLandcoverInit.R
@@ -33,14 +33,13 @@ defineModule(sim, list(
 doEvent.scfmLandcoverInit = function(sim, eventTime, eventType, debug=FALSE) {
   if (eventType=="init") {
     
-    sim <- scfmLandcoverInitCacheFunctions(sim)
+    #sim <- scfmLandcoverInitCacheFunctions(sim)
     sim <- scfmLandcoverInitInit(sim)
     sim <- scheduleEvent(sim, params(sim)$scfmLandcoverInit$.plotInitialTime,
                          "scfmLandcoverInit", "plot")
     sim <- scheduleEvent(sim, params(sim)$scfmLandcoverInit$.saveInitialTime,
                          "scfmLandcoverInit", "save")
   } else if (eventType=="plot") {
-    #browser()
     Plot(sim$vegMap, new=TRUE)
     Plot(sim$flammableMap, legend=FALSE) # this is failing probably due to a bug in Plot
     # EJM is working on it 20160224
@@ -56,69 +55,81 @@ doEvent.scfmLandcoverInit = function(sim, eventTime, eventType, debug=FALSE) {
   return(invisible(sim))
 }
 
-genFireMapAttr<-function(sim){
+genFireMapAttr<-function(flammableMap, studyArea, neighbours){
   #calculate the cell size, total area, and number of flammable cells, etc.
   #
   #All areas in ha
   #
-  cellSize<-prod(res(sim$flammableMap))/1e4 #in ha
+  cellSize<-prod(res(flammableMap))/1e4 #in ha
   
-  ecoregionErrors <- as.numeric(sim$studyArea$ECOREGION)==0
+  ecoregionErrors <- as.numeric(studyArea$ECOREGION)==0
   if(any(ecoregionErrors)) {
     warning("ECOREGION in studyArea has invalid values. Removing invalid values")
-    sim$studyArea <- sim$studyArea[!ecoregionErrors,]
+    studyArea <- studyArea[!ecoregionErrors,]
   }
   
-  if (globals(sim)$neighbours==8)
+  if (neighbours==8)
     w<-matrix(c(1,1,1,1,0,1,1,1,1),nrow=3,ncol=3)
-  else if (globals(sim)$neighbours==4)
+  else if (neighbours==4)
     w<-matrix(c(0,1,0,1,0,1,0,1,0),nrow=3,ncol=3)
   else 
     stop("illegal global neighbours spec")
   #it would be nice to somehow get caching to work on the function argument of focal
   #but I have not been able to make it work.
-  neighMap <- Cache(focal, 1-sim$flammableMap, w, na.rm=TRUE) #default function is sum(...,na.rm)
-  neighMapVals <- getValues(neighMap)
   
-  # extract table for each polygon
-  valsByPoly <- Cache(extract, neighMap, sim$studyArea, cellnumbers = TRUE)
-  
-  names(valsByPoly) <- sim$studyArea$ECOREGION
-  uniqueZoneNames <- unique(sim$studyArea$ECOREGION)
-  valsByZone <- lapply(uniqueZoneNames, function(ecoName) {
-    aa <- valsByPoly[names(valsByPoly)==ecoName] 
-    if(is.list(aa)) aa <- do.call(rbind, aa)
-    aa
-  })
-  names(valsByZone) <- uniqueZoneNames
-  
-  # Derive frequency tables of number of flammable cells, per polygon type, currently ECOREGION
-  nNbrs <- lapply(valsByZone, function(x) {
-    nNbrs<-tabulate(x[,2]+1, 9)#depends on sfcmLandCoverInit
-    names(nNbrs)<-0:8
-    nNbrs
-  })
-  
-  nFlammable <- lapply(valsByZone, function(x) {
-    sum(1-getValues(sim$flammableMap)[x[,1]], na.rm=TRUE)
-  })
-  
-  landscapeAttr <- purrr::transpose(list(cellSize=rep(list(cellSize), length(nFlammable)), 
-                                         nFlammable=nFlammable,
-                                         nNbrs=nNbrs,
-                                         cellsByZone=lapply(valsByZone, function(x) x[,1])))
-  
-  sim$landscapeAttr <- lapply(landscapeAttr, function(x) {
-    append(x, list(burnyArea=x$cellSize*x$nFlammable))
-  })
-  names(sim$landscapeAttr) <- names(valsByZone)
-  sim$cellsByZone <- data.frame(cell=1:ncell(sim$flammableMap), zone=NA)
-  for(x in names(sim$landscapeAttr)) {
-    sim$cellsByZone[sim$landscapeAttr[[x]]$cellsByZone,"zone"] <- x
+  makeLandscapeAttr <- function(flammableMap, weight, studyArea) {
+    neighMap <- Cache(focal, 1-flammableMap, w, na.rm=TRUE) #default function is sum(...,na.rm)
+    #neighMapVals <- getValues(neighMap)
+    
+    # extract table for each polygon
+    valsByPoly <- Cache(extract, neighMap, studyArea, cellnumbers = TRUE)
+    #browser()
+    
+    names(valsByPoly) <- studyArea$ECOREGION
+    uniqueZoneNames <- unique(studyArea$ECOREGION)
+    valsByZone <- lapply(uniqueZoneNames, function(ecoName) {
+      aa <- valsByPoly[names(valsByPoly)==ecoName] 
+      if(is.list(aa)) aa <- do.call(rbind, aa)
+      aa
+    })
+    names(valsByZone) <- uniqueZoneNames
+    
+    # Derive frequency tables of number of flammable cells, per polygon type, currently ECOREGION
+    nNbrs <- lapply(valsByZone, function(x) {
+      nNbrs<-tabulate(x[,2]+1, 9)#depends on sfcmLandCoverInit
+      names(nNbrs)<-0:8
+      nNbrs
+    })
+    
+    nFlammable <- lapply(valsByZone, function(x) {
+      sum(1-getValues(flammableMap)[x[,1]], na.rm=TRUE)
+    })
+    
+    landscapeAttr <- purrr::transpose(list(cellSize=rep(list(cellSize), length(nFlammable)), 
+                                           nFlammable=nFlammable,
+                                           nNbrs=nNbrs,
+                                           cellsByZone=lapply(valsByZone, function(x) x[,1])))
+    
+    landscapeAttr <- lapply(landscapeAttr, function(x) {
+      append(x, list(burnyArea=x$cellSize*x$nFlammable))
+    })
+    names(landscapeAttr) <- names(valsByZone)
+    landscapeAttr
+    
   }
   
+  landscapeAttr <- Cache(makeLandscapeAttr, flammableMap, w, studyArea)
   
-  return(invisible(sim))
+  cellsByZoneFn <- function(flammableMap, landscapeAttr) {
+    cellsByZone <- data.frame(cell=1:ncell(flammableMap), zone=NA_character_)
+    for(x in names(landscapeAttr)) {
+      cellsByZone[landscapeAttr[[x]]$cellsByZone,"zone"] <- x
+    }
+    cellsByZone
+  }
+  cellsByZone <- Cache(cellsByZoneFn, flammableMap, landscapeAttr)
+  
+  return(list(landscapeAttr=landscapeAttr, cellsByZone=cellsByZone))
 }
 
 ### template initilization
@@ -130,16 +141,26 @@ scfmLandcoverInitInit = function(sim) {
   newClass <- ifelse(oldClass %in% nonFlammClasses,1,0)   #1 codes for non flammable 
   #see mask argument for SpaDES::spread()
   flammableTable <- cbind(oldClass, newClass)
-  sim$flammableMap <- ratify(reclassify(sim$vegMap, flammableTable,count=TRUE))
-  if ("Mask" %in% names(objs(sim))){
-    sim$flammableMap <- sim$flammableMap * sim$Mask
+
+  makeFlammableMap <- function(vegMap, flammableTable, lsSimObjs) {
+    flammableMap <- ratify(reclassify(vegMap, flammableTable,count=TRUE))
+    if ("Mask" %in% lsSimObjs){
+      flammableMap <- flammableMap * sim$Mask # don't pass in sim$Mask explicitly to fn so not assessed in Cache
+    }
+    #the count options should cause that "a column with frequencies is added. 
+    
+    #setColors(sim$flammableMap, n=2) <- c("blue","red")
+    setColors(flammableMap,2) <- colorRampPalette(c("blue", "red"))(2) 
+    #flammableMap <- writeRaster(flammableMap, filename = "flammableMap.tif", 
+    #                            datatype = "INT2U", overwrite=TRUE)
+    flammableMap
   }
-  #the count options should cause that "a column with frequencies is added. 
+  sim$flammableMap <- Cache(makeFlammableMap, sim$vegMap, flammableTable, ls(sim))
   
-  #setColors(sim$flammableMap, n=2) <- c("blue","red")
-  setColors(sim$flammableMap,2) <- colorRampPalette(c("blue", "red"))(2) 
   
-  sim<-genFireMapAttr(sim)
+  # This makes sim$landscapeAttr & sim$cellsByZone
+  outs<-Cache(genFireMapAttr, sim$flammableMap, sim$studyArea, globals(sim)$neighbours)
+  list2env(outs, envir = envir(sim)) # move 2 objects to sim environment without copy
   
   return(invisible(sim))
 }
@@ -148,26 +169,26 @@ testFun<-function(x) {
   sum(na.omit(x)==1)
 }
 
-scfmLandcoverInitCacheFunctions <- function(sim) {
-  # for slowp functions, add cached versions
-  # browser()
-  if(params(sim)$scfmLandcoverInit$useCache) {
-    sim$cacheLoc <- file.path(outputPath(sim), "scfmLandcoverInitCache") 
-    if(!dir.exists(sim$cacheLoc) )
-      createEmptyRepo(file.path(outputPath(sim), "scfmLandcoverInitCache"))
-    
-    sim$focal <- function(...) {
-      archivist::cache(cacheRepo=sim$cacheLoc, FUN=raster::focal, ...)
-    }
-    
-    # sim$Frabjous <-function(...){
-    #   archivist::cache(cacheRepo=sim$cacheLoc, FUN=sim$testFun, ...)
-    # }
-    
-  } else {
-    #sim$Frabjous <- function(x){sum(na.omit(x)==1)}
-    sim$focal <- raster::focal
-  }
-  
-  return(invisible(sim))
-}
+# scfmLandcoverInitCacheFunctions <- function(sim) {
+#   # for slowp functions, add cached versions
+#   # browser()
+#   if(params(sim)$scfmLandcoverInit$useCache) {
+#     sim$cacheLoc <- file.path(outputPath(sim), "scfmLandcoverInitCache") 
+#     if(!dir.exists(sim$cacheLoc) )
+#       createEmptyRepo(file.path(outputPath(sim), "scfmLandcoverInitCache"))
+#     
+#     sim$focal <- function(...) {
+#       archivist::cache(cacheRepo=sim$cacheLoc, FUN=raster::focal, ...)
+#     }
+#     
+#     # sim$Frabjous <-function(...){
+#     #   archivist::cache(cacheRepo=sim$cacheLoc, FUN=sim$testFun, ...)
+#     # }
+#     
+#   } else {
+#     #sim$Frabjous <- function(x){sum(na.omit(x)==1)}
+#     sim$focal <- raster::focal
+#   }
+#   
+#   return(invisible(sim))
+# }
