fb0266071b9b595c3ebd1da5385dc40ea64ef4e0
SteveCumming <stevec.boreal@gmail.com>
2018-10-17 20:51:00 -0400

    Upgraded scfmSpread code to current standards of beauty

--- modules/scfmSpread/scfmSpread.R
+++ modules/scfmSpread/scfmSpread.R
@@ -20,22 +20,17 @@ defineModule(sim, list(
     defineParameter("startTime", "numeric", 0, NA, NA, desc="Simulation time at which to initiate burning"),
     defineParameter(".plotInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first plot event should occur"),
     defineParameter(".plotInterval", "numeric", NA, NA, NA, "This describes the simulation time at which the first plot event should occur"),
-    defineParameter(".saveInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
-    defineParameter(".saveInterval", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
+    #defineParameter(".saveInitialTime", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
+    #defineParameter(".saveInterval", "numeric", NA, NA, NA, "This describes the simulation time at which the first save event should occur"),
     defineParameter("neighbours", "numeric", 8, NA, NA, "Number of immediate cell neighbours")
   ),
-  inputObjects = data.frame(
-    objectName = c("scfmPars","spreadState","flammableMap"),
-    objectClass = c("list","data.table","RasterLayer"),
-    sourceURL = "",
-    other = NA_character_,
-    stringsAsFactors = FALSE
+  inputObjects = bind_rows(
+    expectsInput(objectName = "scfmPars", objectClass = "list", desc = ""),
+    expectsInput(objectName = "spreadState", objectClass = "data.table", desc = ""),
+    expectsInput(objectName = "flammableMap", objectClass = "RasterLayer", desc = "")
   ),
-  outputObjects = data.frame(
-    objectName = "burnMap",
-    objectClass = "RasterLayer",
-    other = NA_character_,
-    stringsAsFactors = FALSE
+  outputObjects = bind_rows(
+    createsOutput(objectName = "burnMap", objectClass = "RasterLayer", desc= "")
   )
 ))
 
@@ -43,63 +38,43 @@ defineModule(sim, list(
 #   - type `init` is required for initialiazation
 
 doEvent.scfmSpread = function(sim, eventTime, eventType, debug = FALSE) {
-  if (eventType == "init") {
-    ### check for more detailed object dependencies:
-    ### (use `checkObject` or similar)
-
-    # do stuff for this event
-    sim <- sim$scfmSpreadInit(sim)
-
-    # schedule future event(s)
-    sim <- scheduleEvent(sim, params(sim)$scfmSpread$startTime, "scfmSpread", "burn")
-    sim <- scheduleEvent(sim, params(sim)$scfmSpread$.plotInitialTime, "scfmSpread", "plot")
-    sim <- scheduleEvent(sim, params(sim)$scfmSpread$.saveInitialTime, "scfmSpread", "save")
-  } else if (eventType == "plot") {
-    # ! ----- EDIT BELOW ----- ! #
-    # do stuff for this event
-    # sim$burnMap[sim$burnMap[]>0]<-1
-
-    Plot(sim$burnMap, title="Fire map", legendRange=c(0,1), 
-         #cols=c("white", "red"), 
-         new=FALSE)#length(sim$ignitionLoci)))
-    sim <- scheduleEvent(sim, time(sim) + params(sim)$scfmSpread$.plotInterval,
-                         "scfmSpread", "plot")
-   
-    # ! ----- STOP EDITING ----- ! #
-  } else if (eventType == "save") {
-    
-  } else if (eventType == "burn") { 
-    if (!is.null(sim$spreadState)){ #we really want to test if the data table has any rows
-      if(any(sim$spreadState$active))
-        sim<-scfmSpreadBurnemup(sim)
-    }
-    
-    sim <- scheduleEvent(sim, time(sim) + params(sim)$scfmSpread$returnInterval, "scfmSpread", "burn")
-
-  }  else {
+  switch(
+    eventType,
+    init = {
+      sim <-Init(sim)
+      # schedule future event(s)
+      sim <- scheduleEvent(sim, P(sim)$startTime, "scfmSpread", "burn")
+      sim <- scheduleEvent(sim, P(sim)$.plotInitialTime, "scfmSpread", "plot")
+    },
+    plot = {
+      Plot(sim$burnMap, title="Fire map", legendRange=c(0,1), new=FALSE)
+      sim <- scheduleEvent(sim, time(sim) + P(sim)$.plotInterval, "scfmSpread", "plot")
+    },
+    burn = { 
+      if (!is.null(sim$spreadState)){  #we really want to test if the data table has any rows
+        if(any(sim$spreadState$active))
+          sim<-Burnemup(sim)
+      }
+      sim <- scheduleEvent(sim, time(sim) + params(sim)$scfmSpread$returnInterval, "scfmSpread", "burn")
+    },
     warning(paste("Undefined event type: '", events(sim)[1, "eventType", with = FALSE],
                   "' in module '", events(sim)[1, "moduleName", with = FALSE], "'", sep = ""))
-  }
+  )
   return(invisible(sim))
 }
 
-## event functions
-#   - follow the naming convention `modulenameEventtype()`;
-#   - `modulenameInit()` function is required for initiliazation;
-#   - keep event functions short and clean, modularize by calling subroutines from section below.
-
-### template initialization
-scfmSpreadInit <- function(sim) {
+Init <- function(sim) {
   
-  sim$burnMap <- sim$flammableMap * 0  # 0 * NA = NA
+  sim$burnMap <- sim$flammableMap 
+  sim$burnMap[] <- sim$flammableMap[] * 0  # 0 * NA = NA
   
   if("scfmPars" %in% ls(sim)) {
     if(length(sim$landscapeAttr) > 1) {
       pSpread <- raster(sim$flammableMap)
       for(x in names(sim$landscapeAttr)) {
         pSpread[sim$landscapeAttr[[x]]$cellsByZone] <- sim$scfmPars[[x]]$pSpread
-        pSpread[] <- pSpread[] * (1-sim$flammableMap[])
-      }
+      }     
+      pSpread[] <- pSpread[] * (1-sim$flammableMap[])
     } else {
       pSpread <- sim$scfmPars[[1]]$pSpread
     }
@@ -108,52 +83,26 @@ scfmSpreadInit <- function(sim) {
     pSpread <- P(sim)$pSpread
   }
   sim$pSpread <- pSpread
-  
-  
-  # pSpread <- ifelse("scfmPars" %in% ls(sim),
-  #            sim$scfmPars$pSpread,
-  #            params(sim)$scfmSpread$pSpread
-  #           )
-  
   setColors(sim$burnMap,n=2) <- colorRampPalette(c("transparent", "red"))(2)
-  
-  # ! ----- STOP EDITING ----- ! #
-
   return(invisible(sim))
 }
 
-scfmSpreadBurnemup <- function(sim){ #name is a homage to Walters and Hillborne
+Burnemup <- function(sim){ #name is a homage to Walters and Hillborne
 
   maxSizes <- unlist(lapply(sim$scfmPars, function(x) x$maxBurnCells))
   activeLoci <- unique(sim$spreadState$initialLocus) # indices[sim$spreadState$active]
+  #we prevent multiple ignitions, which shouldn't happen anyway.
   maxSizes <- maxSizes[sim$cellsByZone[activeLoci,"zone"]]
   
   sim$burnDT <- SpaDES.tools::spread(sim$flammableMap,
                                      spreadProb = sim$pSpread,
                                      spreadState = sim$spreadState,
-                                     #mask=sim$flammableMap, #this should work but it don't
                                      directions = P(sim)$neighbours,
-                                     maxSize = maxSizes, 
+                                     maxSize = maxSizes,  #not sure this works 
                                      returnIndices = TRUE, 
                                      id = TRUE)
   sim$burnMap[sim$burnDT$indices] <- 1
-  idx<-which(sim$burnMap[] != 0)
-  if (length(idx)>0){
-      sim$ageMap[idx] <- 0
-  }
+  sim$ageMap[sim$burnDT$indices] <- 0
   return(invisible(sim))
 }
-### template for save events
-scfmSpreadSave <- function(sim) {
-  sim <- saveFiles(sim)
-  return(invisible(sim))
-}
-
-### template for plot events
-scfmSpreadPlot <- function(sim) {
-  return(invisible(sim))
-}
-
-
-### add additional events as needed by copy/pasting from above
 
